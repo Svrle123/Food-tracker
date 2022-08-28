d5a3a0acce949afd1a7e392e7f13dbf3f6b961ab
Ivan Svrtan <ivan.svrtan@gmail.com>
2022-08-28 23:58:14 +0200

    Added redux store and userSlice

---
+++ client/src/features/user/userSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { IUser } from '../../core/interfaces/IUser';
+
+const initialState: IUser = {
+    userName: "",
+    password: "",
+    isAdmin: false,
+    email: "",
+    name: "",
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        logIn: (state: IUser, action: PayloadAction<IUser>) => {
+            return { ...state, ...action.payload };
+        },
+        logOut: () => {
+            return { ...initialState };
+        },
+    },
+})
+
+export const { logIn, logOut } = userSlice.actions
+
+export default userSlice.reducer
